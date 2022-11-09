5e11451f23a13e155fb37ea11793009bae95f1f2
Fernando Bozzo <fdbozzo@atsistemas.com>
2022-11-09 16:49:46 +0100

    Actualización de los scripts schema.sql y data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,24 @@
+-- Agregar datos a la BBDD BRANDS
+INSERT INTO brands (id, name) VALUES (1, 'Zara');
+
+-- Agregar datos a la BBDD PRICES
+INSERT INTO prices
+  (brand_id, product_id, start_date, end_date, priority, price, price_list, curr)
+  VALUES
+  (1, 35455, TIMESTAMP WITH TIME ZONE'2020-06-14T00:00:00.000+00:00', TIMESTAMP WITH TIME ZONE'2020-12-31T23:59:59.000+00:00', 0, 35.35, 1, 'EUR');
+
+INSERT INTO prices
+  (brand_id, product_id, start_date, end_date, priority, price, price_list, curr)
+  VALUES
+  (1, 35455, TIMESTAMP WITH TIME ZONE'2020-06-14T15:00:00.000+00:00', TIMESTAMP WITH TIME ZONE'2020-06-14T18:30:00.000+00:00', 1, 25.45, 1, 'EUR');
+
+INSERT INTO prices
+  (brand_id, product_id, start_date, end_date, priority, price, price_list, curr)
+  VALUES
+  (1, 35455, TIMESTAMP WITH TIME ZONE'2020-06-15T00:00:00.000+00:00', TIMESTAMP WITH TIME ZONE'2020-06-15T11:00:00.000+00:00', 1, 30.50, 1, 'EUR');
+
+INSERT INTO prices
+  (brand_id, product_id, start_date, end_date, priority, price, price_list, curr)
+  VALUES
+  (1, 35455, TIMESTAMP WITH TIME ZONE'2020-06-15T16:00:00.000+00:00', TIMESTAMP WITH TIME ZONE'2020-12-31T23:59:59.000+00:00', 1, 38.95, 1, 'EUR');
+
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS brands (
+  id INT NOT NULL AUTO_INCREMENT  PRIMARY KEY,
+  name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS prices (
+  brand_id INT NOT NULL,
+  product_id INT NOT NULL,
+  start_date TIMESTAMP(3) WITH TIME ZONE NOT NULL,
+  end_date TIMESTAMP(3) WITH TIME ZONE NOT NULL,
+  priority TINYINT NOT NULL,
+  price DOUBLE PRECISION NOT NULL,
+  price_list SMALLINT NOT NULL,
+  curr CHAR(3) NOT NULL
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS Prices_Composite_Key ON prices(brand_id, product_id, start_date, end_date, priority)
